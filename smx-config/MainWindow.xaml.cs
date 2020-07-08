4bae4054a764e2088e8fc7ad1ed5cf87dc4aeb03
BenouKat <ben.goze@gmail.com>
2020-07-08 10:12:07 +0200

    Select main tab first

--- smx-config/MainWindow.xaml.cs
+++ smx-config/MainWindow.xaml.cs
@@ -169,7 +169,7 @@ namespace smx_config
         {
             InitializeComponent();
 
-            onConfigChange = new OnConfigChange(this, delegate (LoadFromConfigDelegateArgs args)
+             onConfigChange = new OnConfigChange(this, delegate (LoadFromConfigDelegateArgs args)
             {
                 LoadUIFromConfig(args);
             });
@@ -222,6 +222,8 @@ namespace smx_config
                 if(result == MessageBoxResult.No)
                     e.Cancel = true;
             };
+
+            Main.SelectedItem = MainTab;
         }
 
         bool IsThresholdSliderShown(string type)
